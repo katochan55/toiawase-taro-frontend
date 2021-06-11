53788f5e704f6542fcadd1cbf35457a6a78d1699
kodai-kato <greatvoyage0122@gmail.com>
2021-06-11 15:38:57 +0900

    WIP

---
+++ components/ClientOnly.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+type Props = {
+  children: xxx;
+  delegated: xxx;
+};
+
+export default function ClientOnly({ children, ...delegated }) {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <div {...delegated}>{children}</div>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
 import Head from "next/head";
 
-export default function Home() {
+export default function Home({ samples }) {
   return (
     <div className="container">
       <Head>
@@ -18,33 +20,14 @@ export default function Home() {
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {/* {countries.map((country) => (
+            <div key={country.code} className="card">
+              <h3>{country.name}</h3>
+              <p>
+                {country.code} - {country.emoji}
+              </p>
+            </div>
+          ))} */}
         </div>
       </main>
 
@@ -206,4 +189,24 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      # query Samples {
+      #   countries {
+      #     code
+      #     name
+      #     emoji
+      #   }
+      # }
+    `,
+  });
+
+  return {
+    props: {
+      countries: data.countries.slice(0, 4),
+    },
+  };
 }
